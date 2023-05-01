f830a0145be9f040928af5e34b57aac61b1c2775
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-05-01 13:15:36 +0100

    The great reset

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
-extern crate bitflags;
-extern crate log;
 
 use std::io::{stdout, Write};
 use std::rc::Rc;
 
-/// Initialise logging based on the current environment
-fn init_logging() {
-    env_logger::try_init().unwrap();
-}
-
 /// This is where the fun starts
 fn main() {
-    init_logging();
+    println!("Chisel")
 }

c3797199449921b28904524b3d05bdc15f5c74ca
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-05-02 07:43:17 +0100

    Nothing interesting, just checking import of cli options

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
+use clap::Parser;
+use cli::Cli;
 use std::io::{stdout, Write};
 use std::rc::Rc;
 
+mod cli;
+
 /// This is where the fun starts
 fn main() {
-    println!("Chisel")
+    let _cli = Cli::parse();
 }

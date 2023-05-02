5a7aba65c4f3d2bb6cc1effa6e752c42dffe4c82
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-05-02 13:06:25 +0100

    Started scaffolding up the default (pretty print) functionality

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,9 @@
 use clap::{crate_version, Command, Parser};
 
+/// Top level command line arguments and configuration settings
 #[derive(Parser)]
 #[command(name = "chisel")]
 #[command(author = "Jonny Coombes <jcoombes@jcs-software.co.uk>")]
-#[command(about = "JSON wrangler", long_about = None)]
+#[command(about = "A simple command line JSON wrangler", long_about = None)]
 #[command(version = crate_version!())]
-pub struct Cli {}
+pub struct Arguments {}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,49 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-use clap::Parser;
-use cli::Cli;
-use std::io::{stdout, Write};
-use std::rc::Rc;
-
+use chisel_json::errors::ParserResult;
+use chisel_json::events::{Event, Match};
+use chisel_json::sax::Parser as SaxParser;
+use clap::Parser as ClapParser;
+use cli::Arguments;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
 mod cli;
+
+fn on_sax_event(event: &Event) -> ParserResult<()> {
+    match &event.matched {
+        Match::StartOfInput => (),
+        Match::EndOfInput => (),
+        Match::StartObject => println!("{{"),
+        Match::ObjectKey(key) => print!("{} : ", key),
+        Match::EndObject => println!("}}"),
+        Match::StartArray => println!("["),
+        Match::EndArray => println!("]"),
+        Match::String(val) => println!("{},", val),
+        Match::Integer(val) => println!("{},", val),
+        Match::Float(val) => println!("{},", val),
+        Match::Boolean(val) => println!("{},", val),
+        Match::Null => todo!(),
+    }
+    Ok(())
+}
+
+fn process(source: &[u8]) {
+    let parser = SaxParser::default();
+    match parser.parse_bytes(source, &mut on_sax_event) {
+        Ok(_) => std::process::exit(0),
+        Err(_) => std::process::exit(1),
+    }
+}
 
 /// This is where the fun starts
 fn main() {
-    let _cli = Cli::parse();
+    let mut buffer: Vec<u8> = vec![];
+    let _args = Arguments::parse();
+    let stdin = io::stdin();
+    let mut reader = BufReader::new(stdin);
+    match reader.read_to_end(&mut buffer) {
+        Ok(_) => process(buffer.as_slice()),
+        Err(_) => (),
+    }
 }

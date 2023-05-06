4e3ffb3ec52f1b5a1ba1089e5732af38c0618255
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-05-06 06:06:54 +0100

    wip: doc comments

--- src/threads.rs
+++ src/threads.rs
@@ -1,11 +1,12 @@
 use std::sync::mpsc::Sender;
 use std::thread::JoinHandle;
 
-use crate::render::commands::DisplayList;
+use crate::render::commands::CommandList;
 
 /// Structure combining a [JoinHandle] for an executing thread, along with
 /// channel information to communicate with the thread
 pub struct AppThread<Message, Result> {
+    /// The [JoinHandle] used to wait for thread completion/cancellation etc...
     pub handle: Option<JoinHandle<Result>>,
     /// One end of a channel to send messages of a specific type to the contained thread
     pub sink: Sender<Message>,
@@ -20,5 +21,5 @@ impl<Message, Result> AppThread<Message, Result> {
 /// Struct to hold global application state etc...
 pub struct AppThreads {
     /// The current rendering thread
-    pub renderer: AppThread<DisplayList, ()>,
+    pub renderer: AppThread<CommandList, ()>,
 }

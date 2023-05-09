4b7b20ec673dbac5a3d906e8c0cb336fa7f16148
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-05-09 19:42:53 +0100

    wip: added object filtration argument started fleshing out the core functionality

--- src/commands/pointers.rs
+++ src/commands/pointers.rs
@@ -1,23 +1,61 @@
 use super::{Command, CommandContext};
-use crate::cl_immediate;
 use crate::errors::ChiselResult;
 use crate::render::display_lists::{DisplayList, DisplayListCommand, DisplayListMode, Draw};
 use crate::sources::{source_from_file, source_from_stdin};
+use chisel_json::errors::ParserResult;
+use chisel_json::events::Event;
 use chisel_json::sax::Parser as SaxParser;
-use clap::Args;
-use std::collections::HashSet;
+use clap::{Args, ValueEnum};
 use std::path::PathBuf;
 
 /// An [Command] responsible for filtering the input
 #[derive(Debug, Args)]
 pub struct PointersCommand {
-    /// Source JSON file. If not specified, input is assumed to come from stdin.
+    /// Source JSON file.
+    ///
+    /// If not specified, input is assumed to come from stdin.
     #[arg(last = true, value_name = "FILE")]
     pub file: Option<PathBuf>,
+
+    /// Pointer types.
+    ///
+    /// If specified, only the JSON pointers corresponding to these specific element
+    /// types will be emitted
+    #[arg(value_enum, long, value_name = "TYPES", value_delimiter = ',')]
+    pub types: Vec<PointerType>,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum PointerType {
+    /// object elements
+    Objects,
+    /// array elements
+    Arrays,
+    /// object member key elements
+    Keys,
+    /// string value elements
+    Strings,
+    /// numeric value elements
+    Numbers,
+    /// boolean value elements
+    Booleans,
+    /// null value elements
+    Nulls,
+}
+
+impl PointersCommand {
+    /// All the SAX event processing passes through here
+    fn handle_sax_event(&self, evt: &Event) -> ParserResult<()> {
+        match evt.pointer {
+            Some(p) => println!("{}", p.to_string()),
+            None => (),
+        }
+        Ok(())
+    }
 }
 
 impl Command for PointersCommand {
-    fn execute(&self, context: &mut CommandContext) -> ChiselResult<()> {
+    fn execute(&self, _context: &mut CommandContext) -> ChiselResult<()> {
         // sort out some argument related stuff and populate the buffer
         let mut buffer: Vec<u8> = vec![];
         if let Some(path) = &self.file {
@@ -26,23 +64,10 @@ impl Command for PointersCommand {
             source_from_stdin(&mut buffer)?;
         }
 
-        let mut pointers = HashSet::new();
+        // instantiate a SAX parser instance and process the input, by delegating
+        // to the `handle_sax_event` associated function
         let parser = SaxParser::default();
-        let _result = parser.parse_bytes(&buffer, &mut |evt| {
-            match evt.pointer {
-                Some(p) => {
-                    let s = p.as_str();
-                    if !pointers.contains(&s.to_string()) {
-                        let _ = context
-                            .render_pipeline
-                            .send(cl_immediate!(Draw::Text(format!("{}", p)), Draw::NewLine));
-                        pointers.insert(s.to_string());
-                    }
-                }
-                None => (),
-            }
-            Ok(())
-        });
+        let _result = parser.parse_bytes(&buffer, &mut |evt| self.handle_sax_event(evt));
 
         Ok(())
     }
